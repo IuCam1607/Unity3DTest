0afbede25f74e024723e5ebbac3f2a81041bccf3
IuCam1607 <pokerf1412@gmail.com>
2023-10-30 21:24:59 +0700

    add radius

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
+    public float radius;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OntriggerEnter" + other.name);
