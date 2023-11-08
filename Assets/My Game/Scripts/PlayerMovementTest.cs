80f7f888dff62cfc270d52041f270e3af870a464
MAY11\MAY11 <pokerf1412@gmail.com>
2023-11-08 21:34:40 +0700

    [add] simple player movement

---
+++ Assets/My Game/Scripts/PlayerMovementTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementTest : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float turnSpeed;
+    private float horizontalInput;
+    private float verticalInput;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementDirection = new Vector3 (horizontalInput, 0, verticalInput);
+
+        movementDirection.Normalize();        //chuẩn hóa Vector
+
+        transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
+
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * Time.deltaTime);// xoay hướng nhân vật
+        }
+    }
+}
