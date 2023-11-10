fb652e261a7aaa1a7bf96b211a86ccd7384cae2c
MAY11\MAY11 <pokerf1412@gmail.com>
2023-11-10 21:25:32 +0700

    [add] character controller

--- Assets/My Game/Scripts/PlayerMovementTest.cs
+++ Assets/My Game/Scripts/PlayerMovementTest.cs
@@ -1,31 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovementTest : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float turnSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpButtonGracePeriod;
+
     private float horizontalInput;
     private float verticalInput;
-    
+    private float yForce;
+
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressTime;
+
+    private CharacterController characterController;
 
 
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     void Update()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movementDirection = new Vector3 (horizontalInput, 0, verticalInput);
 
+        float magnitude = movementDirection.magnitude;
+        magnitude = Mathf.Clamp01(magnitude);
+
         movementDirection.Normalize();        //chuẩn hóa Vector
 
-        transform.Translate(movementDirection * moveSpeed * Time.deltaTime, Space.World);
+        yForce += Physics.gravity.y * Time.deltaTime;
+
+        if(characterController.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jumpButtonPressTime = Time.time;
+        }    
+
+        if(Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        {
+            yForce = -0.5f;
+            characterController.stepOffset = originalStepOffset;
+            if (Time.time - jumpButtonPressTime <= jumpButtonGracePeriod)
+            {
+                yForce = jumpForce;
+                jumpButtonPressTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+
+        Vector3 velocity = moveSpeed * magnitude * movementDirection;
+        velocity.y = yForce;
+
+        characterController.Move(velocity * Time.deltaTime);
 
         if(movementDirection != Vector3.zero)
         {
